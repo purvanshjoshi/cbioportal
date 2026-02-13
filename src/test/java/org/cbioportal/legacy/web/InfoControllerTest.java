d19b52b9ad6aadf78f829a91575b0c1dc16076c8
Purvansh Joshi <purvanshjoshi7534011576@gmail.com>
2026-02-14 01:51:25 +0530

    Refactor InfoControllerTest to JUnit 5

--- src/test/java/org/cbioportal/legacy/web/InfoControllerTest.java
+++ src/test/java/org/cbioportal/legacy/web/InfoControllerTest.java
@@ -5,8 +5,7 @@ import static org.mockito.Mockito.when;
 import org.cbioportal.legacy.model.InfoDb;
 import org.cbioportal.legacy.service.InfoService;
 import org.cbioportal.legacy.web.config.TestConfig;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
@@ -14,12 +13,10 @@ import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.TestPropertySource;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
-@RunWith(SpringJUnit4ClassRunner.class)
 @WebMvcTest
 @ContextConfiguration(classes = {InfoController.class, TestConfig.class})
 @TestPropertySource(
